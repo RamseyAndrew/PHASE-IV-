19ba97226a36f6cdf8afaecb095aeaade9b56a95
RamseyAndrew <anti.xe17@gmail.com>
2025-10-03 15:22:20 +0300

    added game models

--- backend/app/models.py
+++ backend/app/models.py
@@ -8,7 +8,11 @@ class Player(db.Model):
     name = db.Column(db.String(50), nullable=False)
     score = db.Column(db.Integer, default=0)
 
-    
+class Game(db.Model):
+    __tablename__ = "games"
+    id = db.Column(db.Integer, primary_key=True)
+    status = db.Column(db.String(20), default="ongoing")  # ongoing, finished, etc.
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
 
     def __repr__(self):
         return f"<Player {self.name}>"
