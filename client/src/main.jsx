ebb6408e3242fa982a923902c563544ed61755bb
RamseyAndrew <anti.xe17@gmail.com>
2025-10-05 16:33:44 +0300

    fixing imports still

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
-import './App.css';
+import './components/App.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
